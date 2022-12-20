e8187b6628c87f7cc5a7b5eb1b0b3d1f9ef304fe
Felipe Carvalho Prette <61854726+darkLoki3@users.noreply.github.com>
2022-12-20 08:58:10 -0300

    Add files via upload

--- Portfolio/Work/Assistente/rosto/rosto.py
+++ Portfolio/Work/Assistente/rosto/rosto.py
@@ -1,17 +1,16 @@
-#! /usr/bin/python
 import pyglet
 
-animation = pyglet.resource.animation('falando2.gif')
-animation2 = pyglet.image.load_animation('piscando2.gif')
-animation3 = pyglet.image.load_animation('erro2.gif')
+animation = pyglet.image.load_animation('falando.gif')
+#animation2 = pyglet.image.load_animation('piscando2.gif')
+#animation3 = pyglet.image.load_animation('erro2.gif')
 animSprite = pyglet.sprite.Sprite(animation)
-animSprite2 = pyglet.sprite.Sprite(animation2)
-animSprite3 = pyglet.sprite.Sprite(animation3)
+#animSprite2 = pyglet.sprite.Sprite(animation2)
+#animSprite3 = pyglet.sprite.Sprite(animation3)
 
 w = animSprite.width
 h = animSprite.height
 
-window = pyglet.window.Window(w, h, fullscreen=True)
+window = pyglet.window.Window(width=1600, height=900, fullscreen=True)
 
 r, g, b, alpha = 0.5, 0.5, 0.8, 0.5
 
