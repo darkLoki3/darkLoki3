e7a64a7ac8821814e15d23d50e701a44077ba0b4
Felipe Carvalho Prette <61854726+darkLoki3@users.noreply.github.com>
2023-01-25 11:31:24 -0300

    Update rosto.py
    Show some gif as face

--- Portfolio/Work/Assistente/rosto/rosto.py
+++ Portfolio/Work/Assistente/rosto/rosto.py
@@ -1,26 +1,38 @@
+import RPi.GPIO as gpio
 import pyglet
 
-animation = pyglet.image.load_animation('falando.gif')
-#animation2 = pyglet.image.load_animation('piscando2.gif')
-#animation3 = pyglet.image.load_animation('erro2.gif')
-animSprite = pyglet.sprite.Sprite(animation)
-#animSprite2 = pyglet.sprite.Sprite(animation2)
-#animSprite3 = pyglet.sprite.Sprite(animation3)
+gpio.setmode(gpio.BCM)
+gpio.setup(29, gpio.IN)
 
-w = animSprite.width
-h = animSprite.height
+def fala():
+    animation = pyglet.image.load_animation('falando.gif')
+    animSprite = pyglet.sprite.Sprite(animation)
+    w = animSprite.width
+    h = animSprite.height
+    window = pyglet.window.Window(width=1600, height=900, fullscreen=True)
+    r, g, b, alpha = 0.5, 0.5, 0.8, 0.5
+    pyglet.gl.glClearColor(r, g, b, alpha)
+    @window.event
+    def on_draw():
+        window.clear()
+        animSprite.draw()
+    pyglet.app.run()
 
-window = pyglet.window.Window(width=1600, height=900, fullscreen=True)
-
-r, g, b, alpha = 0.5, 0.5, 0.8, 0.5
-
-pyglet.gl.glClearColor(r, g, b, alpha)
-
-
-@window.event
-def on_draw():
-    window.clear()
-    animSprite.draw()
-
-
-pyglet.app.run()
+def pisca():
+    animation = pyglet.image.load_animation('piscando.gif')
+    animSprite = pyglet.sprite.Sprite(animation)
+    w = animSprite.width
+    h = animSprite.height
+    window = pyglet.window.Window(width=1600, height=900, fullscreen=True)
+    r, g, b, alpha = 0.5, 0.5, 0.8, 0.5
+    pyglet.gl.glClearColor(r, g, b, alpha)
+    @window.event
+    def on_draw():
+        window.clear()
+        animSprite.draw()
+    pyglet.app.run()
+    
+if(gpio.input(29)==True):
+    fala()
+    else:
+        pisca()
